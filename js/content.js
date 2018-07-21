ec9e04d746c705b4278b82dfa931ae6db19942e4
Kangsan Lee <kanglee@microsoft.com>
2018-07-20 17:13:58 -0700

    Change dom element style from extention

--- js/content.js
+++ js/content.js
@@ -1,11 +1,20 @@
 // get the URL of the page
-var url = document.location.href;
+(function(){
+    var url = document.location.href;
 
-// if not on a docs.microsoft.com domain
-if (url.indexOf("//docs.microsoft.com") <= -1) {
-    // send inactive icons
-    browser.runtime.sendMessage({
-        "iconPath20": "images/inactive20.png",
-        "iconPath40": "images/inactive40.png"
-    });
-}
+    // example code to change color on www.airbnb.com
+    setTimeout(()=>{
+        console.log("timeout");
+        var targetElement = document.getElementById('MagicCarpetSearchBar');
+        targetElement.style.backgroundColor = 'blue';
+    }, 5000);   
+
+    // if not on a docs.microsoft.com domain
+    /*if (url.indexOf("//docs.microsoft.com") <= -1) {
+        // send inactive icons
+        browser.runtime.sendMessage({
+            "iconPath20": "images/inactive20.png",
+            "iconPath40": "images/inactive40.png"
+        });
+    }*/
+})();
